0d59fc146b420b34f86e2e077d281e8dcfdf9041
EphiRobinsonMohawk <000969115@mohawkcollege.ca>
2025-04-15 17:16:32 -0400

    Add Audio Manager

---
+++ Robinson-Ephi-A5/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header ("--Audio Source--")]
+    [SerializeField] AudioSource musicSource;
+
+    [Header("--Audio Clip--")]
+    public AudioClip music;
+}
